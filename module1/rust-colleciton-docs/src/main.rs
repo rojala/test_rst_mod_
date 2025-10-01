aa947f5b2d7ef55b2d5e452e757a8ef14c9005c3
rojala <rami.ojala@iki.fi>
2025-10-01 11:17:29 +0000

    Rust-collection-docs: Small cli app parsing args
    Small Rust command line app that parses arguments
    using Vec, HashMap, and BTreeSet

--- module1/rust-colleciton-docs/src/main.rs
+++ module1/rust-colleciton-docs/src/main.rs
@@ -1,6 +1,57 @@
+use std::collections::{HashMap, BTreeSet};
+use clap::{Arg, Command};
+
 mod lib_benchmark;
 mod lib_priorityqueue;
 mod lib_wordcount;
+
+fn main_with_clap() {
+    let matches = Command::new("rust-collection-docs")
+        .version("1.0")
+        .author("Author Name")
+        .about("Parses command line arguments using Vec, HashMap, and BTreeSet")
+        .arg(
+            Arg::new("args")
+                .help("Arguments to parse")
+                .required(false)
+                .num_args(0..)
+        )
+        .get_matches();
+
+    let args: Vec<String> = matches
+        .get_many::<String>("args")
+        .map(|vals| vals.cloned().collect())
+        .unwrap_or_else(Vec::new);
+
+    parse_args(args);
+}
+
+fn parse_args(args: Vec<String>) {
+    // Store argument counts in a HashMap
+    let mut arg_counts = HashMap::new();
+    // Store unique arguments in a BTreeSet
+    let mut unique_args = BTreeSet::new();
+
+    for arg in &args {
+        *arg_counts.entry(arg.clone()).or_insert(0) += 1;
+        unique_args.insert(arg.clone());
+    }
+
+    println!("Arguments as Vec: {:?}", args);
+    println!("Argument counts (HashMap): {:?}", arg_counts);
+    println!("Unique arguments (BTreeSet): {:?}", unique_args);
+}
+
+fn test_parse_args() {
+    // Simulate command line arguments
+    let args = vec![
+        "foo".to_string(),
+        "bar".to_string(),
+        "foo".to_string(),
+        "baz".to_string(),
+    ];
+    parse_args(args);
+}
 
 fn test_word_count() {
     let text = "Hello world hello";
@@ -37,4 +88,6 @@ fn main() {
     test_word_count();
     test_priority_queue();
     test_benchmark_collections();
+    test_parse_args();
+    main_with_clap();
 }
